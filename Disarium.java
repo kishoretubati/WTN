4c760380d449f017cd9dc1d162e3d41c6fee3a90
kishoretubati <60732676+kishoretubati@users.noreply.github.com>
2020-06-24 08:38:21 +0530

    Add files via upload

---
+++ Disarium.java
@@ -0,0 +1,24 @@
+package numer;
+import java.util.Scanner;
+public class Disarium {
+	public static void main(String[] args) {
+		int a,c=0,d;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		d=a;
+		while(a>0)
+		{
+			c=c+a%10;
+			a=a/10;
+		}
+		if(d%c==0)
+		{
+			System.out.println("its a harshad ");
+		}
+		else
+		{
+			System.out.println("its not a harhsad ");
+		}
+	}
+
+}
